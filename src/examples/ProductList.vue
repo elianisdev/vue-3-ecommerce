1fea28562af8561354a33333a871b98920e53213
Eliana Suancha Guzman <elianitasuanguz@gmail.com>
2025-02-20 18:47:46 -0500

    commit feature composition API

---
+++ src/examples/ProductList.vue
@@ -0,0 +1,31 @@
+<script lang="ts"> //componente usando option API
+import ProductCard from "@/examples/ProductCard.vue";
+
+export default {
+  components: {
+    ProductCard
+  },
+  data(){// data es una funcion que retorna un objeto define el estado del componente y ya se usa en el template
+    return {
+products: [
+  {name: 'Silla', price: 56},
+  {name: 'Mesa', price: 450},
+  {name: 'Escritorio', price: 120}
+]
+    }
+  },
+  methods: {
+
+  }
+
+}
+
+</script>
+
+<template>
+<ProductCard v-for= "product in products" />
+</template> //template es el html del componente lo que se muestra al usuario
+
+<style scoped>
+
+</style>
