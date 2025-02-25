d130fd6a0c08cf49feeacfc0c6c6d253ccf06e54
Eliana Suancha Guzman <elianitasuanguz@gmail.com>
2025-02-24 19:23:39 -0500

    commit component product list

--- src/examples/ProductCard.vue
+++ src/examples/ProductCard.vue
@@ -1,14 +1,34 @@
 <script lang="ts">
+import type { PropType } from "vue";
+import type { Product } from "@/examples/Products.ts";
+
 export default {
-  props: ['product']
-}
+  props: {
+    product: {
+      type: Object as PropType<Product>,
+      required: true
+    }
+  },
+  emits: ['addProduct'],
+  methods : {
+    onAddProduct(productId: number){
+      console.log('Agregando al carrito' + productId);
+
+    }
+    }
+  }
+
 </script>
 
 <template>
- <p>Nombre: {{ product.name}}</p>
+  <p>Nombre: {{ product.name }}</p>
   <p>Precio: {{ product.price }}</p>
+  <button @click="onAddProduct(product.id)">
+    Agregar al carrito
+  </button>
   <hr />
-</template>
-<style scoped>
 
+</template>
+
+<style scoped>
 </style>
