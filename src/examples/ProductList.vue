908329f74cf5215b04c4ce6946a85ecbb3c86de0
Eliana Suancha Guzman <elianitasuanguz@gmail.com>
2025-02-25 12:17:15 -0500

    commit component card

--- src/examples/ProductList.vue
+++ src/examples/ProductList.vue
@@ -1,39 +1,48 @@
-<script lang="ts"> //componente usando option API
+<script lang="ts">
 import ProductCard from "@/examples/ProductCard.vue";
-import type {Product} from "@/examples/Products.ts";
+import CardComponent from "@/examples/CardComponent.vue";
+import type { Product, CartDetail } from "@/examples/types.ts";
 
 export default {
   components: {
-    ProductCard
+    ProductCard,
+    CardComponent
   },
-  data(){// data es una funcion que retorna un objeto define el estado del componente y ya se usa en el template
+  data() {
     return {
-products: <Array<Product>>[
-  {name: 'Silla', price: 56, id: 1},
-  {name: 'Mesa', price: 450, id: 2},
-  {name: 'Escritorio', price: 120, id: 3},
-]
+      products: <Array<Product>>[
+        { name: 'Silla', price: 56, id: 1 },
+        { name: 'Mesa', price: 450, id: 2 },
+        { name: 'Escritorio', price: 120, id: 3 }
+      ],
+      details: <Array<CartDetail>>[]
     }
   },
   methods: {
-    onAddProduct(){
-      console.log('Agregando al carrito');
+    onAddProduct(productId: number) {
+     const detailFound = this.details.find(detail => detail.productId === productId);
+    if (detailFound) {
+      detailFound.quantity++;
+    } else {
+      this.details.push({
+        productId,
+        quantity: 1 });
+    }
+
     }
   }
 }
-
 </script>
 
 <template>
-<ProductCard
-  v-for="product in products"
-  :product="product"
-  @addProduct="onAddProduct"
-
-  :key="product.name" />
-
-</template>//template es una funcion que retorna un objeto define la estructura del componente, `@` escucha un evento es un atajo para `v-on Es la forma corta osea shorthand para v-on:click="onAddProduct(product.id)"`
+  <ProductCard
+    v-for="product in products"
+    :product="product"
+    @addProduct="onAddProduct"
+    :key="product.name"
+  />
+  <CardComponent :details="details"/>
+</template>
 
 <style scoped>
-
 </style>
